e390c46133a1847c9575a6c6a4150e6dec927ed4
tonyStack101 <tonyGithub@gmail.com>
2019-12-08 22:52:54 +0700

    route install

---
+++ resources/views/install.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form action="{{ route('app.installHandle') }}" method="post">
+            <input type="text" placeholder="shop domain" name="shop">
+        </form>
+    </body>
+</html>
